dec6a0eb3bc3c0b292e74fa065f41d8100ec91fa
Sangeetha11o1 <71844465+Sangeetha11o1@users.noreply.github.com>
2023-08-06 20:06:14 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import { Box, styled } from '@mui/material';
+import React from 'react';
+import headerImage from '../images/header.jpg';
+import SearchInputEl from './searchInputEl';
+
+const Header = () => {
+
+  const StyleHeader = styled(Box)(({theme})=>(
+    {
+      display: "flex",
+      justifyContent: 'center',
+      alignItems: 'center',
+      minHeight: 400,
+      backgroundImage: 'url('+headerImage+')',
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundColor: theme.palette.secondary.main
+    }
+
+  ));
+    
+  
+  return (
+    <>
+      <StyleHeader>
+        <SearchInputEl />
+      </StyleHeader>
+      
+    </>
+  )
+}
+
+export default Header
